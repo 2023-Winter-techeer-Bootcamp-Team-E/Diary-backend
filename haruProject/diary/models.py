0129f1071c7deab9c10582aca48a7f80d5186d15
dongwoo <woomacho@gmail.com>
2024-01-03 21:51:51 +0900

    feat: diary models.py 작성

--- haruProject/diary/models.py
+++ haruProject/diary/models.py
@@ -10,7 +10,12 @@ class Diary(models.Model):
     diary_day = models.CharField(max_length=10)
     sns_link = models.URLField(max_length=500)
     diary_bg_url = models.URLField(max_length=500)
-    is_expiry = models.BooleanField(default=False)
+    is_expiry = models.BooleanField(default=False, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
+
+    #calendar
+    # diary_textboxs
+    # diary_stickers
+    # guests
