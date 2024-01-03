61b11705891aa37ae63d5a8ebf11f1d2ea37568e
dongwoo <woomacho@gmail.com>
2024-01-03 21:52:12 +0900

    feat: diary serializers.py 작성

--- haruProject/diary/serializers.py
+++ haruProject/diary/serializers.py
@@ -6,14 +6,14 @@ from .models import Diary
 class DiaryDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Diary
-        exclude = ['deleted_at', 'updated_at']
+        exclude = ['is_deleted', 'updated_at']
 
 
 class DiaryListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Diary
         fields = ['diary_id', 'is_expiry']
-        exclude = ['deleted_at', 'updated_at']
+        exclude = ['is_deleted', 'updated_at']
 
 
 class DiarySnsLinkSerializer(serializers.ModelSerializer):
@@ -26,7 +26,9 @@ class DiaryCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Diary
         fields = ['title', 'diary_date', 'diary_day', 'sns_link', 'diary_bg_url']
-        exclude = ['deleted_at', 'updated_at']
+
+    def create(self, validated_data):
+        return Diary.objects.create(**validated_data)
 
 
 class DiaryFinalSaveSerializer(serializers.ModelSerializer):
@@ -34,5 +36,24 @@ class DiaryFinalSaveSerializer(serializers.ModelSerializer):
     # diary_stickers = Diary_sticker_Serializer(many=True)
     class Meta:
         model = Diary
-        fields = ['diary_date', 'diary_day']
         # textBoxs, stickers
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.diary_date = validated_data.get('diary_date', instance.diary_date)
+        instance.diary_day = validated_data.get('diary_day', instance.diary_day)
+        instance.sns_link = validated_data.get('sns_link', instance.sns_link)
+        instance.diary_bg_url = validated_data.get('diary_bg_url', instance.diary_bg_url)
+        instance.save()
+
+        # # Update or create TextBox instances
+        # textBoxs_data = validated_data.pop('textBoxs', [])
+        # for textBox_data in textBoxs_data:
+        #     TextBox.objects.update_or_create(diary=instance, **textBox_data)
+        #
+        # # Update or create Sticker instances
+        # stickers_data = validated_data.pop('stickers', [])
+        # for sticker_data in stickers_data:
+        #     Sticker.objects.update_or_create(diary=instance, **sticker_data)
+
+        return instance
