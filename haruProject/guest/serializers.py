4636d111055bfe7110fae4e3bd30a97f3de295b9
d0kyoung <ehrud0522@gmail.com>
2024-01-11 01:58:39 +0900

    refacor : 기본스티커 페이지 별 조회 기능 구현 및 guest swagger 적용

--- haruProject/guest/serializers.py
+++ haruProject/guest/serializers.py
@@ -2,22 +2,10 @@ from rest_framework import serializers
 from .models import Guest
 
 
-class GuestSerializer(serializers.Serializer):
-    password = serializers.CharField(write_only=True, max_length=4)
+class GuestCreateSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Guest
+            fields = ['guest_pw', 'diary', 'guest_id']
 
-    def validate_password(self, value):
-        if not value.isdigit() or len(value) != 4:
-            raise serializers.ValidationError("비밀번호는 4자리 숫자여야 합니다.")
-        return value
-
-    def create(self, validated_data):
-        password = validated_data.get('password')
-        guest = Guest(guest_pw=password)
-        guest.save()
-        return {
-            "data": {
-                "guest_id": str(guest.guest_id),
-            },
-            "code": "G001",
-            "message": "게스트 생성 성공"
-        }
+        def create(self, validated_data):
+            return Guest.objects.create(**validated_data)
