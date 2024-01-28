f5619f7650ee27d4e37de5dc5953a9473b17e2ae
dongwoo <woomacho@gmail.com>
2024-01-29 01:59:55 +0900

    fix: diary, calendar 필드, self 수정

--- haruProject/diary/urls.py
+++ haruProject/diary/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path # , re_path
 
 from . import views, consumers
-from .views import DiaryManager, DiaryStickerManager, Diaries, DiariesSave
+from .views import DiaryManager, DiaryStickerManager, Diaries, DiariesSave, HaruRoomManager
 
 #
 # websocket_urlpatterns = [
@@ -11,7 +11,7 @@ from .views import DiaryManager, DiaryStickerManager, Diaries, DiariesSave
 urlpatterns = [
     path('', Diaries.as_view(), name='diaries'),
     #path('save', Diaries.as_view(), name='diary_textbox_manager'),
-    path('save',DiariesSave.as_view()),
+    path('save', DiariesSave.as_view()),
     path('link', DiaryManager.as_view(), name='diary_manager'),
     path('stickers', DiaryStickerManager.as_view(), name='diary_sticker_manager'),
     path("harurooms/<int:diary_id>/", HaruRoomManager.as_view()),
