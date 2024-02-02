3a1a1a65f29713cef81858f11b59672f22f1680e
kingmusung <abuga991008@gmail.com>
2024-02-02 22:15:11 +0900

    guest api 삭제

--- haruProject/config/settings.py
+++ haruProject/config/settings.py
@@ -59,7 +59,6 @@ INSTALLED_APPS = [
     'diary',
     'rest_framework',
     'member',
-    'guest',
     'harucalendar',
     'drf_yasg',
     'storages',
@@ -144,16 +143,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'haru',
-        'USER': 'root',
-        'PASSWORD': '12345678',
-        'HOST': 'db',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'haru',
+#         'USER': 'root',
+#         'PASSWORD': '12345678',
+#         'HOST': 'db',
+#         'PORT': '3306',
+#     }
+# }
 
 # RDS 설정
 # DATABASES = {
@@ -169,12 +168,12 @@ DATABASES = {
 #
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- haruProject/guest/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import GuestCreateView
-
-urlpatterns = [
-    # 기존의 path
-    path('<int:diary_id>/', GuestCreateView.as_view(), name='create_guest')
-]
